ce02d8a8e2d347be4b38e6c7d1684f0d62a0eb12
mpedroni <mpedroni77@gmail.com>
2021-01-16 19:35:22 -0300

    definição da paleta de cores para do tema claro

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,6 +3,18 @@ import Vuetify from 'vuetify/lib/framework';
 
 Vue.use(Vuetify);
 
-const opts = {};
+const opts = {
+  theme: {
+    themes: {
+      light: {
+        primary: '#037f8c',
+        'primary-variant': '#038c8c',
+        accent: '#f27405',
+        'accent-variant': '#f28705',
+        secondary: '#f2c335',
+      },
+    },
+  },
+};
 
 export default new Vuetify(opts);
