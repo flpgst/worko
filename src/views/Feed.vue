8b77374402d7817ade166cc8c82509b6954483e1
Filipe Augusto Gonçalves <flpgst@gmail.com>
2021-01-16 17:29:08 -0300

    View para home

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -1,11 +1,38 @@
 <template>
-  <v-row>
-    <h1>Feed</h1>
-  </v-row>
+  <v-container>
+    <v-row justify="center">
+      <v-col class="d-flex align-start justify-center">
+        <v-btn
+          tile
+          depressed
+          small
+          min-width="200"
+          :class="feed ? 'primary--text' : null"
+          :color="!feed ? 'primary' : null"
+          @click="feed = false"
+          >VAGAS</v-btn
+        >
+        <v-btn
+          tile
+          depressed
+          small
+          :class="!feed ? 'primary--text' : null"
+          :color="feed ? 'primary' : null"
+          @click="feed = true"
+          >POST DA COMUNIDADES</v-btn
+        >
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col> </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    feed: true,
+  }),
+};
 </script>
-
-<style></style>
