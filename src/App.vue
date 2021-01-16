6c464f9c6bc4cea57d221dfa871d435faa542cc3
Filipe Augusto Gonçalves <flpgst@gmail.com>
2021-01-16 13:02:53 -0300

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <!-- <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+      </div> -->
+
+       <v-toolbar-title>
+          WORKO
+          </v-toolbar-title>
+          <v-spacer />
+          <v-toolbar-items>
+          <v-btn
+            dark
+            color="primary"
+            depressed
+            to="/"
+          >
+            <v-icon>mdi-account-outline</v-icon>
+            Feed
+          </v-btn>
+          <v-btn
+            dark
+            color="primary"
+            depressed
+            to="/atividades"
+          >
+            <v-icon>mdi-checkbox-marked-outline</v-icon>
+            Atividades
+          </v-btn>
+          <v-btn
+            dark
+            color="primary"
+            depressed
+            to="/perfil"
+          >
+          <v-icon>mdi-account-outline</v-icon>
+            Perfil
+          </v-btn>
+          </v-toolbar-items>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
