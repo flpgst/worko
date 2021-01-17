491724bcbb8ed151a4f937fd3b35dbce8c6ac685
Filipe Augusto Gonçalves <flpgst@gmail.com>
2021-01-17 01:01:20 -0300

    Concluido Feed

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,34 +1,85 @@
 <template>
-  <v-list three-line>
-    <template v-for="(post, index) in feed">
-      <v-list-item :key="post.titulo">
-        <v-list-item-content>
-          <v-list-item-title v-html="post.titulo"></v-list-item-title>
-          <v-list-item-subtitle v-html="post.descricao"></v-list-item-subtitle>
+  <div>
+    <v-row justify="center" class="ma-0">
+      <v-col cols="11" class="py-0">
+        <v-textarea v-model="post" filled label="Escreva seu post aqui" shaped></v-textarea>
+      </v-col>
+    </v-row>
 
-          <v-row>
-            <v-col cols="2">
-              <v-list-item-action>
+    <v-row class="mx-3 mt-n6" justify="end">
+      <v-col cols="1" class="py-0">
+        <v-btn icon small>
+          <v-icon small>mdi-share-variant</v-icon>
+        </v-btn>
+      </v-col>
+      <v-col cols="1" class="py-0">
+        <v-btn icon small>
+          <v-icon small>mdi-image-size-select-actual</v-icon>
+        </v-btn>
+      </v-col>
+      <v-col cols="auto" class="py-0">
+        <v-btn color="primary" rounded class="font-weight-bold" :disabled="!post" small>
+          <v-icon left>mdi-plus-circle</v-icon>
+          Criar Post
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-list>
+      <v-list-item
+        v-for="{ id, titulo, descricao, data, imagem, autor, comentarios, likes } in feed"
+        :key="id"
+        class="my-3"
+      >
+        <v-card flat color="grey lighten-4" class="px-3 my-2">
+          <v-list-item-content>
+            <v-row class="font-weight-bold">
+              <v-col>
+                {{ titulo }}
+              </v-col>
+            </v-row>
+
+            <v-row class="">
+              <v-col cols="12" class="caption pt-0">
+                Postado por:
+                <strong>{{ autor }} </strong>,
+                {{ data }}
+              </v-col>
+            </v-row>
+
+            <v-row v-if="imagem">
+              <v-col cols="12">
+                <v-img :src="imagem" height="280" :alt="titulo" contain />
+              </v-col>
+            </v-row>
+
+            <v-row>
+              <v-col v-text="descricao" class="text-justify" />
+            </v-row>
+
+            <v-row>
+              <v-col cols="auto">
                 <v-btn icon>
-                  <v-icon> mdi-thumb-up-outline </v-icon>
-                  {{ post.likes }}
+                  <v-icon>mdi-thumb-up-outline</v-icon>
+                  {{ likes }}
                 </v-btn>
-              </v-list-item-action>
-            </v-col>
-            <v-col cols="2">
-              <v-list-item-action>
+              </v-col>
+              <v-col cols="2">
                 <v-btn icon>
                   <v-icon>mdi-comment-text-outline</v-icon>
-                  {{ post.comentarios }}
+                  {{ comentarios }}
                 </v-btn>
-              </v-list-item-action>
-            </v-col>
-          </v-row>
-          <v-divider :key="index"></v-divider>
-        </v-list-item-content>
+              </v-col>
+              <v-col cols="2">
+                <v-btn icon>
+                  <v-icon>mdi-bookmark-multiple-outline</v-icon>
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-list-item-content>
+        </v-card>
       </v-list-item>
-    </template>
-  </v-list>
+    </v-list>
+  </div>
 </template>
 
 <script>
@@ -37,6 +88,7 @@ import feed from '../database/feed';
 export default {
   data: () => ({
     feed,
+    post: null,
   }),
 };
 </script>
--- src/database/feed.js
+++ src/database/feed.js
@@ -1,10 +1,26 @@
+import Imagem1 from './images/feed/1.png';
+
 export default [
   {
-    titulo: 'Li o livro Bras Cubas',
-    descricao: 'Gostei muito, vcs sabiam que esse e um clássico da literatura brasileira?',
+    id: 1,
+    titulo: 'Desafio de Leitura Worko - Como fazer amigos e influenciar pessoas',
+    descricao:
+      'Gente acabei de finalizar o desafio de leitura daqui da Worko e vim compartilhar algumas das lições que aprendi com esse livro!Para quem ainda não fez o desafio de leitura desse mês, não sabe o livro incrivel que está perdendo.Aqui estão algumas das lições do livro:Princípio 1: nunca critique, condene ou reclame.Sabe aquela pessoa que vive reclamando da vida? Para ela, sempre tem algo ruim. Criticar os outros, então, se tornou rotina. E isso afeta aqueles que estão ao redor. Todos ficam para baixo e as vítimas da crítica ficam ressentidas e com o orgulho ferido....',
+    imagem: Imagem1,
     autor: 'João da Silva',
+    data: '05/01/2021',
     likes: 5,
     comentarios: 8,
+  },
+  {
+    id: 2,
+    titulo: 'Alguem poderia me ajudar a revisar minha carta de apresentação?',
+    descricao:
+      'Me sinto muito travada na hora de escrever sobre mim, gostaria de uma ajuda para revisar minha carta de apresentação.Como ainda não tenho experiência profissional, fiz alguns dos cursos que estão na aba Educação e Desenvolvimento e agora estou tentando descrever as habilidades e projetos que desenvolvi durantes esses cursos.Segue abaixo a carta de apresentação que escrevi: ....',
+    autor: 'Luana Braga',
+    data: '05/01/2021',
+    likes: 34,
+    comentarios: 21,
   },
   {
     titulo: 'Vcs viram a palestra do Marcelo Tás??',
