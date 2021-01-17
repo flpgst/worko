306f4e92d533812030258cad7cf45ed5007eaf69
mpedroni <mpedroni77@gmail.com>
2021-01-17 14:45:08 -0300

    Finalizado componente de comentarios

---
+++ src/database/comentarios.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    id: 1,
+    comentarios: [
+      {
+        texto: 'Eu tbn li!! Achei incrível',
+        autor: 'Maria Alice',
+        resposta: 'Demais neh?',
+      },
+      {
+        texto: 'sdsadsadsa',
+        autor: 'Jhony',
+        resposta: 'Legal',
+      },
+    ],
+  },
+  {
+    id: 2,
+    comentarios: [
+      {
+        texto: 'Superr!!',
+        autor: 'Jorginho',
+        resposta: 'Yeahh',
+      },
+    ],
+  },
+];
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import Feed from '@/views/Feed.vue';
+import Comentarios from '@/views/Comentarios.vue';
 
 Vue.use(VueRouter);
 
@@ -10,6 +11,14 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Feed,
+  },
+  {
+    path: '/comentarios/:postId',
+    name: 'Comentarios',
+    component: Comentarios,
+    props: ({ params }) => ({
+      postId: params.postId,
+    }),
   },
 ];
 
