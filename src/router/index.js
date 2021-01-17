cbc5ed7b9dd301c3b3cddff58a64251f32e70a34
mpedroni <mpedroni77@gmail.com>
2021-01-17 16:47:21 -0300

    remoção de rota do Perfil por conta de conflitos de versão do arquivo

--- src/router/index.js
+++ src/router/index.js
@@ -33,11 +33,6 @@ const routes = [
     name: 'educacao-e-desenvolvimento',
     component: Educacao,
   },
-  {
-    path: '/perfil',
-    name: 'Perfil',
-    component: Perfil,
-  },
 ];
 
 const router = new VueRouter({
